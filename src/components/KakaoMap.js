d7da0d17b5a4151f276ecb9688f62666eb8788e0
yseulgi <ysg213@petprice.kr>
2024-03-13 12:02:16 +0900

    지도 좌표 수정 및 확대/축소 안되게 막기

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,30 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const { kakao } = window;
 
 function KakaoMap() {
+  const [isZoomable, setIsZoomable] = useState(false);
   useEffect(() => {
     if (typeof window !== "undefined" && kakao) {
       const container = document.getElementById("map");
       const options = {
-        center: new kakao.maps.LatLng(37.454012, 126.707794),
-        level: 3,
+        center: new kakao.maps.LatLng(35.15066798081444, 126.85810830157614),
+        level: 2,
       };
-      const markerPosition = new kakao.maps.LatLng(37.454012, 126.707794);
+      const markerPosition = new kakao.maps.LatLng(
+        35.15066798081444,
+        126.85810830157614
+      );
       const marker = new kakao.maps.Marker({
         position: markerPosition,
       });
 
       const map = new kakao.maps.Map(container, options);
       marker.setMap(map);
+
+      map.setZoomable(isZoomable);
     } else {
       console.error("Kakao Map API가 로드되지 않았습니다.");
     }
-  }, []);
+  }, [isZoomable]);
+
+  useEffect(() => {
+    console.log(isZoomable);
+  }, [isZoomable]);
 
   return (
     <>
       {kakao ? (
-        <div id="map" style={{ width: "100%", height: "100%" }}></div>
+        <div
+          style={{ width: "100%", height: "100%", minHeight: "400px" }}
+          // onClick={() => setIsZoomable(!isZoomable)}
+        >
+          <div id="map" style={{ width: "100%", height: "100%" }}></div>
+        </div>
       ) : (
         <div
           style={{
